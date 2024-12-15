c338f63065d2127a13d6c5bcf926cfabb86cdae8
josemariajorquia <jomarjorquia5@gmail.com>
2024-12-15 21:58:24 +0800

    code_challenge2.py

--- code_challenge2.py
+++ code_challenge2.py
@@ -1,3 +1,5 @@
-def code_chal2():
+def cc2():
     name = input("What is your name? ")
     print("\t\t\t\t\t\t\t\t\t\t\t\t      * \n\t\t\t\t\t\t\t\t\t\t\t\t    * * * \n\t\t\t\t\t\t\t\t\t\t\t\t  * * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t*Hi, " + name + "* \n\t\t\t\t\t\t\t\t\t\t\t\t  * * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t    * * * \n\t\t\t\t\t\t\t\t\t\t\t\t      *")
+
+cc2()
