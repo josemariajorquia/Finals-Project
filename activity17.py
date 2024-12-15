247cbe2a6b48c22fa6c97e8759c0d102cfc5ee23
josemariajorquia <jomarjorquia5@gmail.com>
2024-12-15 08:54:53 +0800

    Add files via upload

---
+++ activity17.py
@@ -0,0 +1,7 @@
+def act17():
+    col = eval(input("Enter number of columns: "))
+    for x in range(1,11):
+        for y in range(1,col+1):
+            print(f"{x} x {y} = {x * y}",end="\t")
+        print()
+act17()
