41d8c23001bc4838f667b757c79c06c11aab9729
josemariajorquia <jomarjorquia5@gmail.com>
2024-12-15 08:40:11 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,7 @@
+def act11():
+    for x in range(1, 10):
+        print("Hello world")
+        print("Happy foundation")
+        isMarried = False
+        print(f"{isMarried} That i am married ")
+act11()
