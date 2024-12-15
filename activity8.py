f870f7de437e921b11299ff2f88490f2d9f76ec3
josemariajorquia <jomarjorquia5@gmail.com>
2024-12-15 08:37:11 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,14 @@
+def act8():
+    password = input("Enter your password: ")
+
+    if password.lower() == "pogi":
+        print("Acces granted")
+        print("Enjoy using the system")
+
+    elif password.lower() == "gwapo":
+        print("Enjoy using the system")
+
+    else:
+        print("Access denied")
+
+act8()
