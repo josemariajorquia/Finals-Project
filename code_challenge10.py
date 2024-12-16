682e51556be258d3aa8a876128920e3d53d5ffb2
josemariajorquia <jomarjorquia5@gmail.com>
2024-12-16 15:42:32 +0800

    Update code_challenge10.py

--- code_challenge10.py
+++ code_challenge10.py
@@ -1,21 +1,21 @@
 def cc10():
 
-for x in range(6,0, -1):
-    for u in range (1,x+1):
-        print(" " , end= " ")
-    for v in range(6,x, -1):
-        print("*", end = " ")
-    for y in range(6,x, -1):
-        print("*", end = " ")
-    print()
-    
-for z in range(1,6):
-    for u in range(1,z+1):
-        print(" " , end=" ")
-    for v in range(6,z, -1):
-        print("*", end = " ")
-    for y in range (6,z,-1):
-        print("*", end = " ")
-    print()
+    for x in range(6,0,-1):
+        for y in range(1,x+1):
+            print(" ", end = " ")
+        for z in range(6,x,-1):
+            print("*", end = " ")
+        for i in range(6,x,-1):
+            print("*", end = " ")        
+        print() 
+        
+    for a in range(1,6):
+        for b in range(1,a+1):
+            print(" ", end = " ")
+        for c in range(6,a,-1):
+            print("*", end= " ")
+        for d in range(6,a,-1):
+            print("*", end= " ")
+        print()   
 
 cc10()
