ea14e93c1e07ad70d8a8813b90d849fa9d79daef
josemariajorquia <jomarjorquia5@gmail.com>
2024-12-16 15:44:15 +0800

    Update code_challenge12.py

--- code_challenge12.py
+++ code_challenge12.py
@@ -1,20 +1,20 @@
 def cc12():
 
-for x in range(6,0, -1) :
-	for u in range (1,x+1):
-		print(" ", end= "")
-	for v in range (5,x, -1) :
-		print("*", end= "")
-	for y in range (5,x, -1):
-		print("*", end= "")
-	print()
-for z in range(4,0, -1):
-	for u in range (1,5-2):
-		print(" ", end= " ")
-	for v in range (6,z, -5):
-		print ("*", end= "") 
-	for y in range (6,z, -5):
-		print ("*", end= "")
-	print()
+    for x in range(6,0,-1):
+        for y in range(1,x+1):
+            print(" ", end= "")
+        for z in range(5,x,-1):
+            print("*", end= "")
+        for z in range(5,x,-1):
+            print("*", end= "")
+        print()
+    for x in range(4,0,-1):
+        for y in range(1,5-2):
+            print(" ",end= " ")
+        for z in range(6,x,-5): 
+            print("*",end= "")
+        for z in range(6,x,-5): 
+            print("*",end= "")    
+        print()  
 
-cc12()
+cc12()  
