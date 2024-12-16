73fcd3225a43895e1422abd6204960210df15e7c
josemariajorquia <jomarjorquia5@gmail.com>
2024-12-16 15:44:55 +0800

    Update code_challenge13.py

--- code_challenge13.py
+++ code_challenge13.py
@@ -1,20 +1,20 @@
 def cc13():
 
-for x in range (1,6):
-	for u in range (6,x, -1) :
-		print(" ", end= " ")
-	for v in range(x,1,-1) :
-		print(v, end = " ")
-	for y in range (1, x+1):
-		print(y, end= " ")
-	print()
-for z in range (4,0,-1):
-	for u in range(6,z, -1):
-		print(" ", end= " ")
-	for v in range(z, 1,-1):
-		print(v, end= " ")
-	for y in range (1,z+1):
-		print(y, end= " ")
-	print()
+    for x in range(1,6):
+        for y in range(6,x,-1):
+            print(" ", end= " ")
+        for z in range(x,1,-1): 
+            print(z, end = " ")
+        for a in range(1,x+1):
+            print(a, end= " ")
+        print()  
+    for a in range(4,0,-1):
+        for b in range(6,a,-1):
+            print(" ", end= " ") 
+        for c in range(a,1,-1):
+            print(c, end= " ")  
+        for d in range(1,a+1):
+            print(d, end= " ")
+        print()    
 
-cc13():
+cc13() 
